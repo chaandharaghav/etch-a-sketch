3d3fbe17afaf67d3489ebea08fe04ff8595e596d
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-07-23 14:51:31 +0530

    Implement way to change board color

--- app.js
+++ app.js
@@ -26,9 +26,13 @@ pointerColorSelector.addEventListener('change', redefineActiveElement)
 
 
 
+const board = document.querySelector('.board');
+
+// function to create the divs in the board
+const tilePicker = document.querySelector('#numTiles')
+
 const createDivs = function () {
-    const numTiles = document.querySelector('#numTiles').value;
-    const board = document.querySelector('.board')
+    const numTiles = tilePicker.value;
     
     //craeting elements to append to board
     for(let i=0; i < numTiles**2; i++){
@@ -47,3 +51,32 @@ const createDivs = function () {
 
 // On DOMContentLoaded, run createDivs once
 document.addEventListener('DOMContentLoaded', createDivs)
+
+
+// if user changes no of tiles value, createDivs again
+const deleteDivs  = function () {
+    board.innerHTML = ''
+}
+tilePicker.addEventListener('change', deleteDivs)
+tilePicker.addEventListener('change', createDivs)
+
+
+// function to set board color
+
+
+// implemented by changing written css, 
+// changing styleSheets will impact this as well
+const findDivBgCss = function () {
+    cssRules = document.styleSheets[0].cssRules;
+    for(let cssRule of cssRules){
+        if(cssRule.selectorText === '.board > div'){
+            return cssRule.style
+        }
+    }
+}
+
+boardColorPicker = document.querySelector('#boardColor')
+boardColorPicker.addEventListener('change', function () {
+    const divBgCss = findDivBgCss()
+    divBgCss.backgroundColor = boardColorPicker.value;
+})
