f8309071caf07b7fcad4d74a26146f3ad902ab4a
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-07-23 21:30:14 +0530

    Add greyscale functionality

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ const randomColor = function () {
 };
 
 // find next rainbow color
-let currentRainbow = "red";
+let currentRainbow = "red"; // setting last element, as when lines below execute, the color becomes violet
 const findNextRainbow = function () {
   const rainbowColors = [
     "violet",
@@ -116,10 +116,77 @@ const findNextRainbow = function () {
   return currentRainbow;
 };
 
-// function to find the next color
-const findCurrentColor = function () {
-  const currentActive = findCurrentActive();
+const parseRgb = function (rgb) {
+  rgb = rgb.substring(4, rgb.length - 1); // removing brackets and the "rgb" key word
+  rgb = rgb.split(","); // splitting the numbers into array
+  rgb = rgb.map((elem) => elem.trim()); // trimming the whitespaces
+  return rgb;
+};
 
+const rgbToHsl = function (rgb) {
+  const [r, g, b] = parseRgb(rgb);
+  let r1 = r / 255;
+  let g1 = g / 255;
+  let b1 = b / 255;
+
+  let cmax = Math.max(r1, g1, b1);
+  let cmin = Math.min(r1, g1, b1);
+  let del = cmax - cmin;
+
+  let hue = 0;
+  if (del === 0) {
+    hue = 0;
+  } else if (cmax === r1) {
+    hue = 60 * (((g1 - b1) / del) % 6);
+  } else if (cmax === g1) {
+    hue = 60 * ((b1 - r1) / del + 2);
+  } else if (cmax === b1) {
+    hue = 60 * ((r1 - g1) / del + 4);
+  }
+
+  let lightness = (cmax + cmin) / 2;
+
+  let saturation = 0;
+  if (del === 0) {
+    saturation = 0;
+  } else if (del > 0 || del < 0) {
+    saturation = del / (1 - Math.abs(2 * lightness - 1));
+  }
+
+  // after calculation saturation and lightness will be in percentage form,
+  // so rounding them and changing to 100% scale
+  hue = Math.round(hue);
+  lightness = Math.round(lightness * 100);
+  saturation = Math.round(saturation * 100);
+
+  return {
+    h: hue,
+    s: saturation,
+    l: lightness,
+  };
+};
+
+const increaseGrey = function (rgb) {
+  let h = rgbToHsl(rgb).h;
+  let s = rgbToHsl(rgb).s;
+  let l = rgbToHsl(rgb).l;
+  return `hsl(${h}, ${s}%, ${l - 5 > 0 ? l - 5 : 0}%)`;
+};
+
+// finding next greyscale
+const findNextGrey = function (elem) {
+  if (elem.style.backgroundColor === "") {
+    // taken from stylesheet, background of .board > div
+    return increaseGrey("rgb(112, 99, 99)");
+  } else {
+    // console.log(increaseGrey(elem.style.backgroundColor));
+    return increaseGrey(elem.style.backgroundColor);
+  }
+};
+
+// function to find the next color
+const findCurrentColor = function (elem) {
+  const currentActive = findCurrentActive();
   switch (currentActive) {
     case "manual":
       return pointerColorSelector.value;
@@ -133,6 +200,9 @@ const findCurrentColor = function () {
     case "rainbow":
       return findNextRainbow();
       break;
+    case "greyscale":
+      return findNextGrey(elem);
+      break;
   }
 };
 
@@ -141,7 +211,9 @@ const changeBgColor = function () {
   let childNodes = document.querySelectorAll(".board > *");
   for (let child of childNodes) {
     child.addEventListener("mouseover", function () {
-      this.style.backgroundColor = findCurrentColor();
+      let color = findCurrentColor(this);
+      this.style.backgroundColor = color;
+      console.log(color);
     });
   }
 };
